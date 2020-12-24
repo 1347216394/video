286c3162dc5cd76f89736dd056c2293999a16f7c
陈666 <chen6813051@163.com>
2020-12-24 11:08:07 +0800

    视频管理上传出现BUG已修改

---
+++ src/main/java/com/qf/service/impl/SpeakerServiceImpl.java
@@ -0,0 +1,8 @@
+package com.qf.service.impl;
+
+import com.qf.service.SpeakerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpeakerServiceImpl implements SpeakerService {
+}
